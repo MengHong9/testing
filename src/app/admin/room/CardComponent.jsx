bb0f8c3015f89e263d6b0d2138a4841dfae131a9
Mr.Frog <Bunmenghong7@gmail.com>
2025-05-27 10:04:23 +0700

    test

---
+++ src/app/admin/room/CardComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardComponent = () => {
+  return (
+ <>   
+  <h1></h1>
+  <h2></h2></>
+  );
+}
+
+export default CardComponent;
---
+++ src/app/admin/room/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardComponent from './CardComponent';
+
+const page = () => {
+  return (
+    <div>
+      hello
+      <CardComponent></CardComponent>
+    </div>
+  );
+}
+
+export default page;
